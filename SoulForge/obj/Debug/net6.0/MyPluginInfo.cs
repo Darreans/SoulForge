77102958a8454d6d91495930a16a19f6cf5718b3
Darrean <95978260+Darreans@users.noreply.github.com>
2025-06-09 23:51:46 -0400

    Delete SoulForge directory

--- SoulForge/obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SoulForge
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SoulForge";
-        public const string PLUGIN_NAME = "SoulForge";
-        public const string PLUGIN_VERSION = "0.1.10";
-    }
-}
